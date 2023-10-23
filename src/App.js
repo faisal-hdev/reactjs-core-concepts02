e50703852d33102390187765bc125b14d6d9b9b6
faisal-hdev <itsfaisalchowdhury@gmail.com>
2023-10-23 20:01:51 +0600

    secound commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-
   return (
     <div className="App">
       <Counter></Counter>
+      <ExternalUsers></ExternalUsers>
     </div>
   );
 };
 
+function ExternalUsers() {
+  const [users, setUser] = useState([]);
+  // useEffect(()=>{}, [])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUser(data))
+  },[])
+  return (
+    <div>
+      <h1>External users</h1>
+      <p>{users.length}</p>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  );
+};
+
+function User(props) {
+  return (
+    <div style={{border:'1px solid red', margin:'20px', borderRadius:'10px'}}>
+      <h3>Name : {props.name} </h3>
+      <p>Email : {props.email}</p>
+    </div>
+  )
+}
+
 function Counter() {
   const [count, setCount] = useState(40);
   const increaseCount = () => setCount(count + 1);
-
   const decreaseCount = () => setCount(count - 1);
-  // const increaseCount = () => {
-  //   const newCount = count + 1;
-  //   setCount(newCount)
-  // }
-
   return (
     <div>
       <h1>Count : {count}</h1>
@@ -29,7 +51,21 @@ function Counter() {
   )
 }
 
+function Product(props) {
+  return (
+    <div className='product'>
+      <h3>Name : {props.name}</h3>
+      <p>Price : {props.price}</p>
+    </div>
+  )
+}
+
 /*
+  // const increaseCount = () => {
+  //   const newCount = count + 1;
+  //   setCount(newCount)
+  // }
+
   const products = [
     {name : 'laptop', price:50000},
     {name : 'iphone', price:70000},
@@ -44,17 +80,6 @@ function Counter() {
    products.map(product => <Product name={product.name} price={product.price}></Product>)
  } 
  */
-
-
-
-function Product(props) {
-  return (
-    <div className='product'>
-      <h3>Name : {props.name}</h3>
-      <p>Price : {props.price}</p>
-    </div>
-  )
-}
 
 export default App;
 
