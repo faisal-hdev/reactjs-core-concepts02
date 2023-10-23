b59f0612b0f8445ab9c4e54341e3b3f563ddb79e
faisal-hdev <itsfaisalchowdhury@gmail.com>
2023-10-23 19:31:01 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter></Counter>
     </div>
   );
+};
+
+function Counter() {
+  const [count, setCount] = useState(40);
+  const increaseCount = () => setCount(count + 1);
+
+  const decreaseCount = () => setCount(count - 1);
+  // const increaseCount = () => {
+  //   const newCount = count + 1;
+  //   setCount(newCount)
+  // }
+
+  return (
+    <div>
+      <h1>Count : {count}</h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
+    </div>
+  )
+}
+
+/*
+  const products = [
+    {name : 'laptop', price:50000},
+    {name : 'iphone', price:70000},
+    {name : 'watch', price:12000},
+    {name : 'tablet', price:25000},
+  ]
+
+{/* <Product name='laptop' price='50000'></Product>
+<Product name='iphone' price='70000'></Product>
+<Product name='Watch' price='12000'></Product>
+ {
+   products.map(product => <Product name={product.name} price={product.price}></Product>)
+ } 
+ */
+
+
+
+function Product(props) {
+  return (
+    <div className='product'>
+      <h3>Name : {props.name}</h3>
+      <p>Price : {props.price}</p>
+    </div>
+  )
 }
 
 export default App;
+
+
+
